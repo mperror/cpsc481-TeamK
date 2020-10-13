951e0fe6df005000d374dc2e1091e5d3870cdff6
Ahmed Hasan <37085550+AMax23@users.noreply.github.com>
2020-10-13 12:56:33 -0600

    Add new portfolio page
    Portfolio created using HTML/CSS

---
+++ main.js
@@ -0,0 +1,13 @@
+
+// This is just to show/hide the menu items.
+$(".Stage1").click(function () {
+    $(".menu .Stage1Show").toggleClass("show");
+});
+
+$(".Stage2").click(function () {
+    $(".menu .Stage2Show").toggleClass("show");
+});
+
+$(".TeamMembers").click(function () {
+    $(".menu .TeamMembersShow").toggleClass("show");
+});
